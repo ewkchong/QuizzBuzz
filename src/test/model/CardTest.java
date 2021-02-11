180604b5b572a7f8c24a4dadc97f89a82670bbee
Edward <eddiewchong@outlook.com>
2021-02-10 23:10:04 -0800

    added tests for methods

--- src/test/model/CardTest.java
+++ src/test/model/CardTest.java
@@ -1,7 +1,31 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
-    // delete or rename this class!
+    Card c;
+
+    @BeforeEach
+    public void runBefore() {
+        c = new Card("","",new ArrayList<>());
+    }
+
+    @Test
+    public void testChangeFront() {
+        assertEquals(c.getFront(), "");
+        c.changeFront("What is the capital city of Canada?");
+        assertEquals(c.getFront(), "What is the capital city of Canada?");
+    }
+
+    @Test
+    public void testChangeBack() {
+        assertEquals(c.getBack(), "");
+        c.changeBack("Ottawa");
+        assertEquals(c.getBack(), "Ottawa");
+    }
 }
---
+++ src/test/model/DeckTest.java
@@ -0,0 +1,76 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckTest {
+    Deck d;
+    ArrayList<Card> cards;
+
+    @BeforeEach
+    public void runBefore() {
+        d = new Deck("CHIN 131");
+        cards = d.getCardList();
+    }
+
+    @Test
+    public void testAddSingleCard() {
+        assertEquals(cards.size(), 0);
+        d.addCard("What is the capital city of China?", "Beijing", new ArrayList<>());
+        assertEquals(cards.size(), 1);
+    }
+
+    @Test
+    public void testAddMultipleCards() {
+        assertEquals(cards.size(), 0);
+        d.addCard("What is the capital city of China?", "Beijing", new ArrayList<>());
+        d.addCard("What is the capital city of Canada?", "Ottawa", new ArrayList<>());
+        d.addCard("What is the capital city of Russia?", "Moscow", new ArrayList<>());
+        assertEquals(cards.size(), 3);
+    }
+
+    @Test
+    public void testDeleteSingleCard() {
+        assertEquals(cards.size(), 0);
+        d.addCard("1+1", "2", new ArrayList<>());
+        assertEquals(cards.size(), 1);
+        d.deleteCard(1);
+        assertEquals(cards.size(), 0);
+    }
+
+    @Test
+    public void testDeleteMultipleCards() {
+        assertEquals(cards.size(), 0);
+        d.addCard("1+1", "2", new ArrayList<>());
+        d.addCard("1+2", "3", new ArrayList<>());
+        d.addCard("1+3", "4", new ArrayList<>());
+        d.addCard("1+4", "5", new ArrayList<>());
+        assertEquals(cards.size(), 4);
+        d.deleteCard(2);
+        assertEquals(cards.size(), 3);
+        d.deleteCard(2);
+        assertEquals(cards.size(), 2);
+        assertEquals(cards.get(0).getBack(), "2");
+        assertEquals(cards.get(1).getBack(), "5");
+    }
+
+    @Test
+    public void testNoDelete() {
+        assertEquals(cards.size(), 0);
+        d.addCard("1+1", "2", new ArrayList<>());
+        assertEquals(cards.size(), 1);
+        d.deleteCard(0);
+        assertEquals(cards.size(), 1);
+    }
+
+    @Test
+    public void testRenameDeck() {
+        assertEquals(d.getTitle(), "CHIN 131");
+        d.renameDeck("CHIN 133");
+        assertEquals(d.getTitle(), "CHIN 133");
+    }
+}
